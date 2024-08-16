713987d322914108bd5e31b90f5e414e38229dc2
Tino Nafiou <nafiou531@gmail.com>
2024-08-16 22:08:20 +0100

    formulaire ok!

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire',
   templateUrl: './formulaire.component.html',
-  styleUrl: './formulaire.component.css'
+  styleUrl: './formulaire.component.css',
 })
 export class FormulaireComponent {
 
-  isValid:boolean = false
-  profilGroup = new FormGroup({
-    user : new FormControl(''),
-    password : new FormControl('')
+  loginForm: FormGroup;
 
-  })
-
-  onValidate(){
-    this.isValid = !this.isValid
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
   }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Logique pour soumettre les données
+      console.log(this.loginForm.value);
+    }
+  }
+
+ 
 }
